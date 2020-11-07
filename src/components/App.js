61d88673e50ac22bf3b7a15844ff9a1d155469b1
ppa21 <pruthviraj_patel@sfu.ca>
2020-11-06 18:37:54 -0800

    testing deployment with this

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>
+                <strong>
+                    App Component 
+                </strong>
+            </h1>
+        );
+    }
+}
+
+export default App;
