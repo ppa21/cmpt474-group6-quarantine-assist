68c2d3374aa68fdb072b9c0fea110a1f6d21313f
psyquia <pesyquia@gmail.com>
2020-11-10 18:17:58 -0800

    Revert "configured aws-exports.js"
    This reverts commit 3afd0ad0773b898eed87a67352138e0ec8aed697.

--- src/components/aws-exports.js
+++ src/components/aws-exports.js
@@ -4,10 +4,10 @@ const awsmobile = {
     region: "us-east-1",
 
     // Amazon Cognito User Pool ID
-    userPoolId: "us-east-1_GPcgwhgV0",
+    userPoolId: "us-east-1_3JTVRoFYi",
 
     // Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: "3d27mke7jqfosk0fqg6qn4in26"
+    userPoolWebClientId: "36ago1h5t7k7ib537bp9gftmac"
   }
 };
 
