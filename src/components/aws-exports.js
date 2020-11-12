f541010e374817c60f6ef68f9797a34be1b82af5
psyquia <59591428+psyquia@users.noreply.github.com>
2020-11-12 14:12:03 -0800

    Update aws-exports.js

--- src/components/aws-exports.js
+++ src/components/aws-exports.js
@@ -4,10 +4,10 @@ const awsmobile = {
     region: "us-east-1",
 
     // Amazon Cognito User Pool ID
-    userPoolId: `${process.env.REACT_APP_USER_POOL_ID}`,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
 
     // Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: `${process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID}`
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
   }
 };
 
