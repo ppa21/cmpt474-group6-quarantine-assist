2b73dd592e05365cd1ea62b05c782fe467eb1f3e
psyquia <pesyquia@gmail.com>
2020-11-11 22:58:24 -0800

    added update attribute functionality

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Auth } from 'aws-amplify';
+import './Profile.css';
+
 
 const Profile = () => {
+  const [user, setUser] = useState({})
   const [attributes, setAttributes] = useState({})
+  const [newAttributes, setNewAttributes] = useState({})
   const attrKeys = {
     'given_name': 'First Name', 
     'family_name': 'Last Name',
@@ -17,25 +21,46 @@ const Profile = () => {
 
   useEffect(() => {
     async function fetchUserAttributes() {
-      const user = await Auth.currentAuthenticatedUser();
-      console.log(user.attributes);
-      setAttributes(user.attributes);
+      const c_user = await Auth.currentAuthenticatedUser()
+      console.log(c_user.attributes);
+      setUser(c_user);
+      setAttributes(c_user.attributes);
+      setNewAttributes(c_user.attributes);
     }
 
     fetchUserAttributes()
   }, [])
 
-  return (
-    <div>
-      <h1>Your Profile</h1>
+  const updateAttributes = async () => {
+    let result = await Auth.updateUserAttributes(user, JSON.stringify(newAttributes))
+    console.log(JSON.stringify(newAttributes))
 
+  }
+
+
+  return (
+    <div className="profile">
+      <h1>Your Profile</h1>
       <div className="all-attributes-container">
         {Object.keys(attrKeys).map((key) => (
           <div className="attribute-container" key={key}>
-      	  <h3>attrKeys[key]</h3>
-          <h4>{(key in attributes) ? attributes[key] : "-"}</h4>
+            <h4>{attrKeys[key]}</h4>
+            <div className="attribute-input">
+              <input
+                type='text' 
+                placeholder={(key in attributes) ? attributes[key] : "-"} 
+                value={newAttributes[key]}
+                onChange={e => {
+                  setNewAttributes({...newAttributes, [key]: e.target.value})
+                  console.log(newAttributes)
+                }} 
+              />
+            </div>
           </div>
         ))}
+        <div className="submit-btn-container">
+          <button className="submit-btn" onClick={updateAttributes}> Submit </button>
+        </div>
       </div>
     </div>
   )
