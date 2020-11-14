f8f7a209e05fac68d6a154be490ad906a64ac2e5
Ida N <inakhost@sfu.ca>
2020-11-14 14:43:50 -0800

    Home page cleanup and minor other fixes
    Added info text on home page
    Some other minor cleanup of style
    Fixed the issue where the sign out option would not show up or disappear unless refreshed
    Added withAuthenticator to new task page

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,9 +12,9 @@ const Main = () => (
   <Switch>
     <Route exact path = "/" component={Home} />
     <Route exact path = "/tasks/all" component={TasksPage} />
-    <Route path = "/videos" component = {Videos} />
+    {/* <Route path = "/videos" component = {Videos} /> */}
     <Route path="/login" exact component={Login} />
-    <Route path="/contact" exact component={Contact} />
+    {/* <Route path="/contact" exact component={Contact} /> */}
     <Route path='/task' component={Task} />
     <Route path='/profile' component={Profile} />
   </Switch>
