c0784b9de89123adc69977104ae7740dbc5b3509
reisele <richard_eisele@sfu.ca>
2020-11-27 10:45:51 -0800

    pull from master

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,6 +6,9 @@ import axios from 'axios'
 import Loader from 'react-loader-spinner'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import "./Task.css"
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const Task = () => {
   const [title, setTitle] = useState('')
@@ -17,6 +20,8 @@ const Task = () => {
   const history = useHistory()
   const location = useLocation()
   const isNewTask = location.pathname.split('/')[2] === 'new'
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   useEffect(() => {
     async function fetchTask() {
@@ -151,6 +156,38 @@ const Task = () => {
     )
   }
 
+  const changeStatus = async e => {
+    e.preventDefault()
+    try {
+      const sessionObject = await Auth.currentSession();
+      const idToken = sessionObject ? sessionObject.idToken.jwtToken : null;
+      const response = await axios.put(
+        `${process.env.REACT_APP_API_URL}/task/${task.id}/status`,
+        {
+          status
+        },
+        {
+          headers: { 'Authorization': idToken }
+        }
+      )
+
+      invalidateTasksCache(idToken);
+
+      console.log(response.data)
+      history.push(`/tasks/all`)
+    } catch (err) {
+      console.error(err)
+    }
+  } 
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
   return (
     <div className="container">
       {!isNewTask && !task.id && <div className="spinner">
@@ -197,6 +234,44 @@ const Task = () => {
             </div>
             <h4>Status: {task.status}</h4>
             {ownsTask
+              ? <div> 
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}> Set Status </Button>
+              <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {/* <MenuItem onClick={handleClose}>Open</MenuItem>
+            <MenuItem onClick={handleClose}>Close</MenuItem>
+            <MenuItem onClick={handleClose}>Help Offered</MenuItem> */}
+     
+            <MenuItem onClick= {e => {
+              setStatus("Open");
+              //updateTask();
+              handleClose();
+            }}
+            >Open</MenuItem>
+
+            <MenuItem onClick= {e => {
+              setStatus("Close");
+              //changeStatus();
+              handleClose();
+            }}
+            >Close</MenuItem>
+
+            <MenuItem onClick= {e => {
+              setStatus("Help Offered");
+              //changeStatus();
+              handleClose();
+            }}
+            >Help Offered</MenuItem>
+          </Menu>
+              </div>
+            : <div className="status-dropdown"></div>
+            }
+            {ownsTask
               ? <textarea
                 className='edit-description'
                 type='text'
@@ -213,6 +288,7 @@ const Task = () => {
         <div className='task-actions'>
           <button onClick={updateTask}>Update task</button>
           <button onClick={deleteTask}>Delete task</button>
+          <button onClick={changeStatus}>Update status</button>
         </div>
       }
     </div>
